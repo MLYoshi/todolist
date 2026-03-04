dab7154d00cce708ef733daa28372d646dc9149b
yoshi <pbl2680658669@gmail.com>
2026-03-04 13:55:50 +0800

    去掉vite.config.js 的rewrite运行顺畅

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host:'0.0.0.0',
+    host: "0.0.0.0",
     proxy: {
       // 代理路径
       "/api": {
-        target: 'http://localhost:5000', // 目标服务器的地址
-        cors: false,
-        changeOrigin: false,
-        rewrite: (path) => path.replace(/^\/api/, " "), // 重写路径
+        target: "http://localhost:8080", // 目标服务器的地址
+        // cors: false,
+        // changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ""), // 重写路径
       },
-    }
-  }
-})
+    },
+  },
+});
